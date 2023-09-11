3261b0117d88e070104c0171abc13943179ccc4b
Chris-code240 <duah.marfochristian@gmail.com>
2023-09-11 13:57:55 +0100

    time

--- app/tests.py
+++ app/tests.py
@@ -2,7 +2,12 @@ from django.test import TestCase
 
 import datetime
 
-print(datetime.datetime.utcnow())
+current_utc_time = datetime.datetime.utcnow().replace(
+            second=0, microsecond=0).isoformat() + 'Z'
+
+current_utc_time = (datetime.datetime.strptime(current_utc_time, "%Y-%m-%dT%H:%M:%SZ") + datetime.timedelta(hours=1)).isoformat() + 'Z'
+
+print(current_utc_time)
 
 
 # Create your tests here.
