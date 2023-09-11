75fcbcaaea19a3416c349c5fd1f9de17531f6976
Christian Duah <82686181+Chris-code240@users.noreply.github.com>
2023-09-11 11:19:35 +0000

    Update Response type

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,14 +10,14 @@ def home(request):
     
     current_datetime = datetime.datetime.now()
     day_of_week = current_datetime.strftime('%A')
-    return json.dumps({ "slack_name": request.GET.get('slack_name','None'),
+    return HttpResponse(json.dumps({ "slack_name": request.GET.get('slack_name','None'),
             "current_day": day_of_week,
             "utc_time": str(datetime.datetime.utcnow()),
             "track": request.GET.get('track','None'),
             "github_file_url": "https://github.com/Chris-code240/Project1/blob/master/app/views.py",
             "github_repo_url": "https://github.com/Chris-code240/Project1",
             "status_code": "200"
-        })
+        }))
 
 #http://example.com/api?slack_name=example_name&track=backend
 # Create your views here.
