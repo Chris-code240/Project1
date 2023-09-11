9b1f6349cd4cb2a795354cff41b06b945f4ab7c0
Chris-code240 <duah.marfochristian@gmail.com>
2023-09-11 13:47:05 +0100

    Json Response Implemented

--- app/views.py
+++ app/views.py
@@ -3,21 +3,36 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 import json
+from django.http import JsonResponse
 import datetime
 
 @api_view(['GET'])
 def home(request):
-    
-    current_datetime = datetime.datetime.now()
-    day_of_week = current_datetime.strftime('%A')
-    return HttpResponse(json.dumps({ "slack_name": request.GET.get('slack_name','None'),
-            "current_day": day_of_week,
-            "utc_time": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-            "track": request.GET.get('track','None'),
-            "github_file_url": "https://github.com/Chris-code240/Project1/blob/master/app/views.py",
-            "github_repo_url": "https://github.com/Chris-code240/Project1",
-            "status_code": 200
-        }))
+        slack_name = request.GET.get('slack_name', 'example_name')
+        track = request.GET.get('track', 'backend')
+
+        # Get the current day of the week
+        current_day = datetime.datetime.now().strftime('%A')
+
+        # Get the current UTC time with a +/-2 minute window
+        current_utc_time = datetime.datetime.utcnow().replace(
+            second=0, microsecond=0).isoformat() + 'Z'
+
+        # Define GitHub repository information
+        github_repo_url = 'https://github.com/Chris-code240/Project1'
+        github_file_url = f'{github_repo_url}/blob/main/file_name.ext'
+
+        # Create the JSON response
+        response_data = {
+            'slack_name': slack_name,
+            'current_day': current_day,
+            'utc_time': current_utc_time,
+            'track': track,
+            'github_file_url': github_file_url,
+            'github_repo_url': github_repo_url,
+            'status_code': 200,
+        }
+        return JsonResponse(response_data)
 
 #http://example.com/api?slack_name=example_name&track=backend
 # Create your views here.
