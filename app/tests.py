c654ce3f949ba08df306f4502651188ec5de7174
Chris-code240 <duah.marfochristian@gmail.com>
2023-09-11 12:55:56 +0100

    Not JSON res

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+import datetime
+
+print(datetime.datetime.utcnow())
+
+
 # Create your tests here.
--- app/views.py
+++ app/views.py
@@ -10,14 +10,14 @@ def home(request):
     
     current_datetime = datetime.datetime.now()
     day_of_week = current_datetime.strftime('%A')
-    return HttpResponse(json.dumps({ "slack_name": request.GET.get('slack_name','None'),
+    return HttpResponse({ "slack_name": request.GET.get('slack_name','None'),
             "current_day": day_of_week,
-            "utc_time": str(datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")),
+            "utc_time": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
             "track": request.GET.get('track','None'),
             "github_file_url": "https://github.com/Chris-code240/Project1/blob/master/app/views.py",
             "github_repo_url": "https://github.com/Chris-code240/Project1",
             "status_code": 200
-        }))
+        })
 
 #http://example.com/api?slack_name=example_name&track=backend
 # Create your views here.
